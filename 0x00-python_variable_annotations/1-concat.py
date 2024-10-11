f539c3e2c0803732e8fb6fe469e72b724a3bf117
[esekyi] <[sskert10@gmail.com]>
2024-10-11 23:57:55 +0000

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""concatening strings which are typed annotated"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A typed-function that takes a string args and returns
+    a str
+    """
+    return str1 + str2
