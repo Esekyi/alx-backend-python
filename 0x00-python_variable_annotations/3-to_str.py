50778b8a10f5931c6772f57f599037e08d4fd4f6
[esekyi] <[sskert10@gmail.com]>
2024-10-11 23:13:21 -0500

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function that accepts n: float
+and returns a str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes n: float as an argument
+    Returns: str
+    """
+    return str(n)
