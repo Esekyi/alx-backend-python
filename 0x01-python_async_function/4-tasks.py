51e14fa1303d1fb5e7c79a58ed4988702e6f51f4
[esekyi] <[sskert10@gmail.com]>
2024-10-15 05:54:44 +0000

    [fixes🛠️] handled tasks concurrently and returned list of delays - 2

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,5 +10,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Spawns task_wait_random n times with the specified max_delay.
     Returns a list of delays in ascending order (without using sort()).
     """
-    delays = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*delays)
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
