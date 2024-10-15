6c18f231758262c671752325e0005d3c8a7ad203
[esekyi] <[sskert10@gmail.com]>
2024-10-15 21:33:41 +0000

    [fixes🛠️] error on list of floats without using sort()

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,5 +12,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
 
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
